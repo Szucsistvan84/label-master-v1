7c66300376a1551e5da79a24057ac9c7f686bdc8
Szucsistvan84 <75862094+Szucsistvan84@users.noreply.github.com>
2026-03-19 16:50:41 +0100

    Update app.py

--- app.py
+++ app.py
@@ -202,7 +202,7 @@ def create_label_pdf(df, fn, ft):
         else:
             # --- MARKETING ETIKETT (Üres helyekre) ---
             p.saveState()
-            p.setDash(1, 2) # Szaggatott vonal a szélére, hogy látszódjon a vágás
+            # p.setDash(1, 2) # Szaggatott vonal a szélére, hogy látszódjon a vágás
             p.setLineWidth(0.1)
             p.rect(x + 1*mm, y + 1*mm, lw - 2*mm, lh - 2*mm, stroke=1, fill=0)
             p.restoreState()

a7edb59eefc335375ba80ea9d565a15ec86a0215
Szucsistvan84 <75862094+Szucsistvan84@users.noreply.github.com>
2026-04-04 15:07:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -231,7 +231,7 @@ def parse_interfood_pdf(pdf_file):
                     admin_name = re.sub(r'\d{3,}', '', admin_name).replace("Ft", "").strip("- /")
 
                     # --- 2. TELEFON ÉS PÉNZ (x47 - x52.5) ---
-                    tel_money_words = [w for w in row_words if x47_4 <= (w['x0'] + w['x1'])/2 < x52_5]
+                    tel_money_words = [w for w in row_words if x47_3 <= (w['x0'] + w['x1'])/2 < x52_5]
                     tel_money_text = " ".join([w['text'] for w in sorted(tel_money_words, key=lambda w: w['x0'])])
                     
                     # Telefon: most már a '20/' is ide fog tartozni

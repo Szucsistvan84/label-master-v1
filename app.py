12c2288b22326e781684b70f14c964ed1874dfa6
Szucsistvan84 <75862094+Szucsistvan84@users.noreply.github.com>
2026-04-19 20:46:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -334,11 +334,20 @@ def parse_interfood_pdf(pdf_file, napi_etlap_kodok):
                 # Fontos: frissítsük a globális nevet is, ha a későbbi pontoknak kell
                 customer_name = local_customer_name
             
-                # A blokk vége: vagy a következő ID, vagy a lap alja (sorompó)
-                next_anchor_top = anchors[i+1]['top'] - 5 if i+1 < len(anchors) else page_cutoff
+                # --- JAVÍTÁS: A blokk vége (Szöveg kettévágásának megakadályozása) ---
+                if i + 1 < len(anchors):
+                    # Ha a következő ID túl közel van, NE vonjunk ki belőle 5 pixelt, 
+                    # mert akkor vízszintesen elvágjuk a szavakat, és vak lesz a program!
+                    if anchors[i+1]['top'] - anchor['top'] < 12:
+                        next_anchor_top = max(anchors[i+1]['top'], anchor['bottom'] + 1)
+                    else:
+                        next_anchor_top = anchors[i+1]['top'] - 1
+                else:
+                    next_anchor_top = page_cutoff
+                
                 y_bottom = min(next_anchor_top, page_cutoff)
                 
-                line_words = [w for w in words if y_top <= w['top'] < y_bottom]
+                line_words = [w for w in words if y_top
                 
                 def get_col_text(x_min, x_max):
                     sel = [w for w in line_words if x_min <= (w['x0'] + w['x1'])/2 < x_max]

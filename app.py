64878812e161cf25f1284e351983b5266a8b4356
Szucsistvan84 <75862094+Szucsistvan84@users.noreply.github.com>
2026-04-26 17:50:16 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1369,9 +1369,9 @@ def create_label_pdf(df, fn, ft, meta, master_df, nevnapok_df, keresztnevek_df, 
             # --- KELLÉK SOR (Marad az emelt helyén) ---
             if kellek_kiiras:
                 p.saveState()
-                p.setFont(f_bold, 6.5)
+                p.setFont(f_reg, 6.5)
                 t_kellek = kellek_kiiras.replace("Kellék:", "").strip()
-                p.drawCentredString(x + lw / 2, y_eff + 12.5 * mm, f"⚠+ {t_kellek} +⚠")
+                p.drawCentredString(x + lw / 2, y_eff + 12 * mm, f"⚠+ {t_kellek} +⚠")
                 p.restoreState()
 
             # --- LEGALJA (Futár vagy Névnap) ---

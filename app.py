163f1aceca39dd45d073f689c0b8f222ef53ccda
Szucsistvan84 <75862094+Szucsistvan84@users.noreply.github.com>
2026-06-13 23:04:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ from adatbazis_modul import (
 )
 from stilus_modul import alkalmaz_mobil_status_bar, alkalmaz_tisztitott_felulet_css, alkalmaz_wolt_gomb_stilus, rendereld_wolt_ugyfel_kartya
 from vizualizacio import utvonal_terkep
-from utils import init_google_sheets, setup_logging, init_test_mode, check_user_role
+from utils import init_google_sheets, setup_logging, init_test_mode, check_user_role, clean_text
 from admin_modul import render_logisztikai_kozpont
 
 # --- KORÁBBI MOBIL ÉS NYOMTATÁS MODULOK BEHÚZÁSA ---
